0e08ea12fd54590e1220c6e7fcc3b526e34813fc
Junior331 <jarilson.rk@gmail.com>
2025-03-14 11:35:37 -0300

    fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -196,7 +196,7 @@ export const Home = () => {
             </div>
           </div>
 
-            {/* <motion.button
+            <motion.button
               className="fixed bottom-24 right-2 flex items-center justify-center border border-white px-6 py-3 font-medium text-white rounded-lg shadow-lg bg-gradient-to-r bg-[#6b7280] focus:outline-none focus:ring-2 focus:ring-offset-2 cursor-pointer"
               initial={{ boxShadow: "0 0 0 0 rgba(156,163,175, 0.7)" }}
               animate={{
@@ -215,9 +215,9 @@ export const Home = () => {
             >
               Agendar
               <span className="absolute top-0 left-0 w-full h-full rounded-lg opacity-0 hover:opacity-20 bg-white transition-opacity duration-300"></span>
-            </motion.button> */}
+            </motion.button>
 
-          <motion.button
+          {/* <motion.button
             className="fixed bottom-24 right-2 flex items-center justify-center border border-white px-6 py-3 font-medium text-white rounded-lg shadow-lg bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
             initial={{ boxShadow: "0 0 0 0 rgba(99, 102, 241, 0.7)" }}
             animate={{
@@ -236,7 +236,7 @@ export const Home = () => {
           >
             Agendar
             <span className="absolute top-0 left-0 w-full h-full rounded-lg opacity-0 hover:opacity-20 bg-white transition-opacity duration-300"></span>
-          </motion.button>
+          </motion.button> */}
         </div>
       </div>
     </Layout>
