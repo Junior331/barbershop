5e6b7b13fec7489a20066384b47ff82dd36d5f01
Junior331 <jarilson.rk@gmail.com>
2025-03-25 01:31:08 -0300

    fix

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -6,7 +6,7 @@ export const Sidebar = () => {
   const location = useLocation();
 
   return (
-    <aside className="flex items-center justify-center w-full">
+    <aside className="flex items-center justify-center w-full z-10">
       <nav className="flex w-full h-[76px] shrink-0 border border-[#F3F4F6] bg-white shadow-[0px_10px_15px_-3px_rgba(0,0,0,0.10),0px_4px_6px_0px_rgba(0,0,0,0.05)]">
         <ul className="flex items-center justify-between px-10 w-full h-full">
           {routes.map((item) => {
