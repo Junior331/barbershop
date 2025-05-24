ac3fe866b3434cc8560aa4c49c2b737ce49b0dd8
Junior331 <jarilson.rk@gmail.com>
2025-05-24 03:50:22 -0300

    Update CircleIcon component to include overflow-hidden class for improved styling and layout consistency.

--- src/components/elements/CircleIcon/index.tsx
+++ src/components/elements/CircleIcon/index.tsx
@@ -5,7 +5,7 @@ export const CircleIcon = ({ children, className }: IProps) => {
   return (
     <div
       className={cn(
-        "flex justify-center items-center min-w-[70px] min-h-[70px] size-[69px] rounded-[70px] border-3 object-cover border-white shadow-[0_0_14px_rgba(0,0,0,0.14)] bg-[#6C8762]",
+        "flex justify-center items-center min-w-[70px] min-h-[70px] size-[69px] rounded-[70px] border-3 object-cover border-white shadow-[0_0_14px_rgba(0,0,0,0.14)] bg-[#6C8762] overflow-hidden",
         className
       )}
     >
